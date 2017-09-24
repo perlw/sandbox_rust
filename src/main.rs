c1e7e9416dce4b7ec66ade66b0f21ff86d4f00c0
Per Lönn Wege <per.loenn.wege@gmail.com>
2017-09-24 16:27:07 +0200

    Regress glfw-sys, should be simplistic, let future wrapper handle prettification

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-extern crate glfw;
+extern crate glfw_sys as glfw;
+mod gl {
+  include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
+use std::ffi::CString;
 
 fn main() {
-  glfw::init();
+  unsafe {
+    glfw::Init();
+    let window = glfw::CreateWindow(640, 480, CString::new("test title").unwrap().as_ptr(), std::ptr::null_mut(), std::ptr::null_mut());
+    glfw::MakeContextCurrent(window);
+    gl::load_with(|s| glfw::GetProcAddress(CString::new(s).unwrap().as_ptr()));
 
-  println!("{}", glfw::get_version_string());
+    gl::ClearColor(0.5, 0.5, 1.0, 1.0);
+    while glfw::WindowShouldClose(window) == 0 {
+      gl::Clear(gl::COLOR_BUFFER_BIT);
 
-  let window = glfw::create_window(640, 480, "test title").unwrap();
-  while !window.should_close() {
-    glfw::poll_events();
-    window.swap_buffers();
+      glfw::PollEvents();
+      glfw::SwapBuffers(window);
+    }
+
+    glfw::DestroyWindow(window);
+    glfw::Terminate();
   }
 }
